e3f2c44ecfb0bf53fd6ff5fd750a207b4f57ced8
ElidallanaCristancho <cristanchodayana062017@gmail.com>
2025-05-14 16:06:24 -0500

    feat: :sparkles: Creacion de la estructura

---
+++ herramienta-app/src/main/java/com/herramienta/herramienta_app/HerramientaAppApplication.java
@@ -0,0 +1,13 @@
+package com.herramienta.herramienta_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerramientaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerramientaAppApplication.class, args);
+	}
+
+}
---
+++ herramienta-app/src/test/java/com/herramienta/herramienta_app/HerramientaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herramienta.herramienta_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerramientaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
