c4d28cafc33ed1bc24ff547ae13b1dd1f8344df9
ElidallanaCristancho <cristanchodayana062017@gmail.com>
2025-05-15 16:28:46 -0500

    feat: :sparkles: Arreglos en controllers

--- src/main/java/com/herramienta/herramienta_app/infrastructure/controllers/ReporteController.java
+++ src/main/java/com/herramienta/herramienta_app/infrastructure/controllers/ReporteController.java
@@ -1,22 +1,31 @@
 package com.herramienta.herramienta_app.infrastructure.controllers;
 
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.herramienta.herramienta_app.application.services.ReporteService;
 
+import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/reporte")
+@RequestMapping("/api/reportes")
+@PreAuthorize("hasRole('ADMIN')")
+@RequiredArgsConstructor
 public class ReporteController {
-     private ReporteService reporteService;
-
-    public ReporteController(ReporteService reporteService) {
-        this.reporteService = reporteService;
+    private final ReporteService reporteService;
+    
+    @GetMapping("/ingresos")
+    public ResponseEntity<ReporteDto> generarReporteIngresos(
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate inicio,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fin) {
+        ReporteDto reporte = reporteService.generarReporteIngresos(inicio, fin);
+        return ResponseEntity.ok(reporte);
     }
-
-    public void generarReporteDeVentas() {
-        reporteService.generarReporteDeVentas();
-    }
-
 }
