3715fb58166bc2e4fdaef49cf2dc49ba00960bac
stivenpe <perezjeisson003@gmail.com>
2025-05-15 14:58:39 -0500

    feat: :zap: finalisation of the discount
    finalisation of the discount

---
+++ src/main/java/com/herramienta/herramienta_app/domain/dtos/HerramientaDto.java
@@ -0,0 +1,69 @@
+package com.herramienta.herramienta_app.domain.dtos;
+
+public class HerramientaDto {
+     private Long id;
+    private String nombre;
+    private String descripcion;
+    private String modelo;
+    private String marca;
+    private double costoPorDia;
+    private int cantidadDisponible;
+    private String categoria;
+    private String proveedor;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    public double getCostoPorDia() {
+        return costoPorDia;
+    }
+    public void setCostoPorDia(double costoPorDia) {
+        this.costoPorDia = costoPorDia;
+    }
+    public int getCantidadDisponible() {
+        return cantidadDisponible;
+    }
+    public void setCantidadDisponible(int cantidadDisponible) {
+        this.cantidadDisponible = cantidadDisponible;
+    }
+    public String getCategoria() {
+        return categoria;
+    }
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    public String getProveedor() {
+        return proveedor;
+    }
+    public void setProveedor(String proveedor) {
+        this.proveedor = proveedor;
+    }
+    
+
+}
---
+++ src/main/java/com/herramienta/herramienta_app/domain/dtos/ReservaDto.java
@@ -0,0 +1,64 @@
+package com.herramienta.herramienta_app.domain.dtos;
+
+import java.time.LocalDate;
+
+public class ReservaDto {
+     private Long id;
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
+    private double costoTotal;
+    private String estado;
+    private Long herramientaId;
+    private Long clienteId;
+    private Long proveedorId;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public LocalDate getFechaInicio() {
+        return fechaInicio;
+    }
+    public void setFechaInicio(LocalDate fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+    public LocalDate getFechaFin() {
+        return fechaFin;
+    }
+    public void setFechaFin(LocalDate fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+    public double getCostoTotal() {
+        return costoTotal;
+    }
+    public void setCostoTotal(double costoTotal) {
+        this.costoTotal = costoTotal;
+    }
+    public String getEstado() {
+        return estado;
+    }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    public Long getHerramientaId() {
+        return herramientaId;
+    }
+    public void setHerramientaId(Long herramientaId) {
+        this.herramientaId = herramientaId;
+    }
+    public Long getClienteId() {
+        return clienteId;
+    }
+    public void setClienteId(Long clienteId) {
+        this.clienteId = clienteId;
+    }
+    public Long getProveedorId() {
+        return proveedorId;
+    }
+    public void setProveedorId(Long proveedorId) {
+        this.proveedorId = proveedorId;
+    }
+
+
+}
