09b42bb639af68e2d4c572dd035e3b9936bee62d
stivenpe <perezjeisson003@gmail.com>
2025-05-16 16:14:23 -0500

    feat: :zap: tests with postman
    tests with postman

--- src/main/java/com/herramienta/herramienta_app/infrastructure/repositories/UsuarioRepository.java
+++ src/main/java/com/herramienta/herramienta_app/infrastructure/repositories/UsuarioRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.herramienta.herramienta_app.domain.entities.Usuario;
 
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByEmail(String email);
     boolean existsByEmail(String email);
